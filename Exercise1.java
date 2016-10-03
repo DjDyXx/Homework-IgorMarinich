a8a747c19d7b59b0b2b6996327f1c92a4b2ffb60
DjDyXx <DjDyXx@users.noreply.github.com>
2016-10-03 13:02:35 +0300

    Add files via upload

--- Exercise1.java
+++ Exercise1.java
@@ -1,17 +1,15 @@
-import java.util.Scanner;
-//Найти результат умножения чисел “a”, “b” и “c”
-// Created by Игорь Маринич on 02.10.2016.
+/**
+ * Created by Игорь on 03.10.2016.
+ */
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println("Для того что-бы умножить числа.Для начала введите число a :");
-        Scanner scanner1 = new Scanner(System.in);
-        int a = scanner1.nextInt();
-        System.out.println("Теперь введите число b :");
-        Scanner scanner2 = new Scanner(System.in);
-        int b = scanner2.nextInt();
-        System.out.println("Теперь введите число c :");
-        Scanner scanner3 = new Scanner(System.in);
-        int c = scanner3.nextInt();
-        System.out.println("Вот ваш результат: " + ( a*b*c));
+        System.out.println("- проснуться");
+        System.out.println("- позавтракать");
+        System.out.println("- одеться");
+        System.out.println("- пойти на работу");
+        System.out.println("- пойти на обед");
+        System.out.println("- вернуться с работы");
+        System.out.println("- поужинать");
+        System.out.println("- лечь спать");
     }
 }
